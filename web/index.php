7888ae5c346b7e0651f3f8fc7e2c7f5fc0f7e4e9
Evert orti <evert.orti@khk.ee>
2017-08-30 12:32:37 +0300

    Created database connection

--- web/index.php
+++ web/index.php
@@ -1,14 +1,27 @@
 <?php
 
+// Using Silex framework
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+// Definitions
 $app['debug'] = true;
 
+
+// Using Twig template framework
 $app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => __DIR__.'/../views',
 ]);
+
+// using Doctrine DBAL
+$app->register(new Silex\Provider\DoctrineServiceProvider(), [
+    'db.options' => [
+        'driver' => 'pdo_sqlite',
+        'path' => __DIR__.'/../database/app.db',
+    ],
+]);
+
 
 $app->get('/bookings/create', function () use ($app) {
     return $app['twig']->render('base.html.twig');
